ab9d7466828d08da8e40e80ff5e4d219e4e60567
Scarlet <lettie@catius.io>
2022-07-10 04:35:10 +0900

    initial config

--- app/src/main/java/io/nuovothoth/nuovothothsamples/presentation/MainActivity.kt
+++ app/src/main/java/io/nuovothoth/nuovothothsamples/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.nuovothoth.nuovothothsamples
+package io.nuovothoth.nuovothothsamples.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.nuovothoth.nuovothothsamples.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
